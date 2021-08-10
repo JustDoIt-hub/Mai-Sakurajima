1590074b937d7c21923f2964054e4003423f8d99
JustDoIt-hub <78211928+JustDoIt-hub@users.noreply.github.com>
2021-08-11 00:07:31 +0530

    Update and rename SaitamaRobot/sample_config.py to config.py

--- config.py
+++ config.py
@@ -15,14 +15,14 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = 2945319  # integer value, dont use ""
+    API_HASH = "b2e2b2f08331f3de995629be0f4e077a"
+    TOKEN = "1939985351:AAHZ52j7qcSGI_JYtJBKEjZS0KGfwSgLHdc"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1687171706  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = 'OnePunchSupport'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -1001190806654  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    SUPPORT_CHAT = 'kyokohorisupport'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -1001523470153  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001523470153  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
